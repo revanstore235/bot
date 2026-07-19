e1f69716fe9818f1eea5c8c815a7fdbb926209d6
revanstore235 <revindradra@gmail.com>
2026-07-19 16:54:44 +0700

    Create index.js to export multiple modules

---
+++ funMenu/index.js
@@ -0,0 +1,9 @@
+const { stalk } = require('./stalk.js');
+const { stalkepep } = require('./stalkepep.js');
+const { logomaker } = require('./logomaker.js');
+
+module.exports = {
+    stalk,
+    stalkepep,
+    logomaker
+};
