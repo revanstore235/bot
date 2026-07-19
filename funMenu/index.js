3cdda942ea1fcbe66927865af4faa7d00d572e94
revanstore235 <revindradra@gmail.com>
2026-07-19 16:56:28 +0700

    Delete funMenu directory

--- funMenu/index.js
+++
@@ -1,9 +0,0 @@
-const { stalk } = require('./stalk.js');
-const { stalkepep } = require('./stalkepep.js');
-const { logomaker } = require('./logomaker.js');
-
-module.exports = {
-    stalk,
-    stalkepep,
-    logomaker
-};
